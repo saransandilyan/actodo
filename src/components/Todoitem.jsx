36400a7e1b8d0bf6ac0028409292b5deea4b392b
saransandilyan <saran111dil@gmail.com>
2024-01-06 01:20:41 +0530

    updated

---
+++ src/components/Todoitem.jsx
@@ -0,0 +1,32 @@
+function TodoItem(props)
+{
+
+    const activityArr=props.activityArr
+    const setActivityArr=props.setActivityArr
+
+    function handleDelete(Deleteid)
+    {
+        var temparr = activityArr.filter(function(item){
+            if(item.id ===Deleteid)
+            {
+                return false
+            }
+            else{
+                return true
+            }
+        })
+
+        setActivityArr(temparr)
+        
+    }
+   
+    return(
+        <div className="flex justify-between">
+        <p>{props.index+1}.{props.item.activity}</p>
+        <button className="text-red-500" onClick={()=>handleDelete(props.id)}>Delete</button>
+        </div>
+    )
+}
+
+
+export default TodoItem
